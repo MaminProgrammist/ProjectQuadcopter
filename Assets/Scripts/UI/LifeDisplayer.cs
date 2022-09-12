a6789708bb61ecb96cee67ab3b2b8ae5a51acd7c
Styopa Groznov <groznov3@gmail.com>
2022-09-12 14:04:49 +0300

    Обновлён UI

--- Assets/Scripts/UI/LifeDisplayer.cs
+++ Assets/Scripts/UI/LifeDisplayer.cs
@@ -1,4 +1,31 @@
-﻿namespace UI
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace UI
 {
-    public class LifeDisplayer : UIDisplayer { }
+    public class LifeDisplayer : MonoBehaviour 
+    {
+        private Image[] _icons;
+
+        private void Awake() => _icons = GetComponentsInChildren<Image>();
+
+        public void Display(int lifes)
+        {
+            for (int i = 0; i < _icons.Length; i++)
+            {
+                Image icon = _icons[i];
+
+                if (i < lifes)
+                {
+                    icon.gameObject.SetActive(true);
+                    continue;
+                }
+
+                icon.rectTransform
+                    .DOPunchScale(Vector3.one, 0.25f, 2, 0)
+                    .OnComplete(() => icon.gameObject.SetActive(false));
+            }
+        }
+    }
 }
